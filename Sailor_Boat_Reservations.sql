17d8210a716e6bd6debd5cad5055e96031ddb21c
Srujan Vaddepally <srujanvaddepally16@gmail.com>
2025-04-28 01:18:14 +0530

    Practice 2/2

--- Sailor_Boat_Reservations.sql
+++ Sailor_Boat_Reservations.sql
@@ -113,32 +113,262 @@
 -- )
 
 -- 6. Find the names of sailors who have reserved all boats.
--- select sname
--- from sailors
--- where sid in(
--- 	select sid
--- 	from Reserves r
--- 	group by sid
--- 	having count(DISTINCT bid) = (
--- 		select count(DISTINCT bid)
--- 		from Boats
--- 	)
--- )
+select sname
+from sailors
+where sid in(
+	select sid
+	from Reserves r
+	group by sid
+	having count(DISTINCT bid) = (
+		select count(DISTINCT bid)
+		from Boats
+	)
+);
 
 -- OR
 
--- SELECT s.sname
--- FROM Sailors s
--- WHERE NOT EXISTS (
---     SELECT *
---     FROM Boats b
---     WHERE NOT EXISTS (
---         SELECT *
---         FROM Reserves r
---         WHERE r.sid = s.sid
---           AND r.bid = b.bid
---     )
--- );
+SELECT s.sname
+FROM Sailors s
+WHERE NOT EXISTS (
+    SELECT *
+    FROM Boats b
+    WHERE NOT EXISTS (
+        SELECT *
+        FROM Reserves r
+        WHERE r.sid = s.sid
+          AND r.bid = b.bid
+    )
+);
+
+-- 7. Find the names of sailors who have reserved all boats whose name starts with “typhoon”.
+select sname
+from Sailors s
+where not exists(
+	select *
+	from boats b
+	where not exists(
+		select *
+		from Reserves r
+		where r.sid=s.sid and r.bid=b.bid
+	)
+	 and b.bname LIKE 'typhoon%'
+);
+
+-- 8.Find the sailor id’s of sailors whose rating is better than some sailor called Bob
+select s1.sid
+from Sailors s1,Sailors s2
+where s2.sname='Bob' and s1.rating > s2.rating ;
+
+
+-- OR
+
+
+SELECT sid
+FROM Sailors
+WHERE rating > ( 
+    SELECT rating
+    FROM Sailors
+    WHERE sname = 'Bob'
+);
+
+-- 9. Find the sailor id’s of sailors whose rating is better than every sailor called Bob.
+select sid
+from sailors
+where rating > all (
+	select rating
+	from sailors
+	where sname='Bob'
+);
+
+-- 10. Find the sailor id(s) of sailors with the highest rating.
+select sid
+from sailors
+where rating = (
+	select max(rating)
+	from sailors
+);
+
+-- 11. Find the name and age of the oldest sailor.
+select sname,age
+from sailors
+where age = (
+	select max(age)
+	from sailors
+);
+
+--or
+
+select sname,age
+from sailors
+where age >= all (
+	select age
+	from sailors
+);
+
+
+-- 12. Find the names of sailors who have reserved every boat reserved by those with a lower rating.
+select sname
+from sailors s1
+where not exists (
+	select 1
+	from sailors s2
+	join reserves r1 on r1.sid=s2.sid
+	join boats b1 on b1.bid=r1.bid
+	where s1.rating>s2.rating and not exists (
+		select 1
+		from reserves r2
+		where r2.sid=s1.sid and r2.bid=b1.bid
+	)
+);
+
+-- NOT EXISTS (SELECT * FROM Boats WHERE NOT EXISTS (reservation))
+
+-- Reads like:
+--  "There is no boat which I didn't reserve."
+
+
+
+-- If you want to check...	Use...
+-- "Has at least one"	EXISTS
+-- "Has none"	NOT EXISTS
+-- "Has everything" (ALL)	NOT EXISTS (NOT EXISTS)
+
+-- 13. For each rating, find the average age of sailors at that level of rating
+select rating,ROUND(avg(age),2)
+from sailors
+group by rating;
+
+-- 14. For each boat which was reserved by at least 5 distinct sailors, find the boat id and the average age of sailors who reserved it.
+select r.bid, ROUND(avg(age),2) as avg_age
+from sailors s
+join reserves r
+on s.sid=r.sid
+join boats b
+on b.bid=r.bid
+group by r.bid
+having count(DISTINCT r.sid) >=5;
+
+--15. For each boat which was reserved by at least 5 sailors with age >= 40, find the boat id and the average age of such sailors.
+
+select r.bid, ROUND(avg(s.age),2) as avg_age
+from sailors s
+join reserves r
+on s.sid=r.sid
+join boats b
+on r.bid=b.bid
+where age >=40
+group by r.bid
+having count(DISTINCT r.sid) >=5;
+
+
+-- SQL queries using EXISTS, NOT EXISTS, IN, NOT IN
+
+-- Find all sailors who have reserved at least one boat.
+select * 
+from sailors s
+where exists (
+	select 1 
+	from reserves r
+	where r.sid=s.sid
+);
+
+
+-- Find all sailors who have not reserved any boat.
+
+select * 
+from sailors s
+where not exists (
+	select 1 -- The SELECT 1 inside the subquery is just a placeholder, meaning "return some value"
+	from reserves r
+	where r.sid=s.sid
+);
+
+
+-- Find the names of all sailors who have reserved a boat that is either 'Red Storm' or 'Blue Thunder'.
+SELECT s.sname
+FROM Sailors s
+WHERE s.sid IN (
+    SELECT r.sid
+    FROM Reserves r
+    JOIN Boats b ON r.bid = b.bid
+    WHERE b.bname IN ('Red Storm', 'Blue Thunder')
+);
+
+-- Find all sailors who have reserved a boat after '2024-04-22'.
+SELECT s.*, r.date
+FROM sailors s
+JOIN reserves r ON s.sid = r.sid
+WHERE r.date > '2024-04-22';
+
+--Using EXISTS
+
+select *
+from sailors s
+where exists (
+	select 1
+	from reserves r
+	where r.sid=s.sid
+	and date > '2024-04-22'
+);
+
+
+-- Find sailors who have reserved boats that are either 'Typhoon One' or 'Typhoon Two'.
+select s.sname
+from sailors s
+where exists (
+	select 1
+	from reserves r
+	join boats b
+	on r.bid=b.bid
+	where s.sid=r.sid
+	and bname in ('Typhoon One','Typhoon Two')
+);
+
+-- using IN
+select sname 
+from sailors
+where sid in (
+	select sid
+	from reserves r
+	join boats b
+	on r.bid =b.bid
+	where bname in ('Typhoon One','Typhoon Two')
+);
+
+
+
+
+
+select sname
+from sailors s1
+where not exists (
+	select 1
+	from sailors s2
+	join reserves r1 on r1.sid=s2.sid
+	join boats b1 on b1.bid=r1.bid
+	where s1.rating>s2.rating and not exists (
+		select 1
+		from reserves r2
+		where r2.sid=s1.sid and r2.bid=b1.bid
+	)
+);
+
+
+
+
+select sname 
+from sailors s
+where exists(
+	select 1
+	from reserves r
+	where s.sid=r.sid
+);
+
+
+
+
+
+
 
 
 
